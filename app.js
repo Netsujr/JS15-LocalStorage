5a5909a0e16e8c5cd0cd3cc809b0c89ed65dae83
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-28 12:51:05 +0900

    checked items is updated and stays on refresh

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
-
-addItems.addEventListener('submit', addItem);
-
 function addItem(e) {
   e.preventDefault();
 
@@ -34,4 +31,18 @@ function populateList(plates = [], platesList) {
   }).join('');
 }
 
+function toggleDone(e) {
+  if (!e.target.matches('input')) return; // skip this unless it's an input
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+
 populateList(items, itemsList);
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+//"plates" class is the parent, responsible for passing on the event
